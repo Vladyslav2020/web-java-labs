69e9c22de91c861dfe842e7c3fd0decb08555aba
VMoskalenko <vladyslav.moskalenko@microfocus.com>
2023-01-09 15:45:13 +0200

    delete unused import

--- lab2/src/main/java/com/kpi/lab2/controllers/web/servlets/LogoutServlet.java
+++ lab2/src/main/java/com/kpi/lab2/controllers/web/servlets/LogoutServlet.java
@@ -1,6 +1,5 @@
 package com.kpi.lab2.controllers.web.servlets;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
